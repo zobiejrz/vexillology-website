be0a18700353aceb95395e2d419274e03d55f17b
Hunter Anderton <dragoneatsslayer@gmail.com>
2019-01-15 10:39:21 -0700

    Picture and siding work

--- docs/javascript/Jay.js
+++ docs/javascript/Jay.js
@@ -1,15 +1,7 @@
-imagesArray = new Array(7);
-imagesArray[0] = new Image();
-imagesArray[0].src = new "usa.png";
-imagesArray[1] = new Image();
-imagesArray[1].src = new "fox.jpg";
-imagesArray[2] = new Image();
-imagesArray[2].src = new "mouse.jpg";
-imagesArray[3] = new Image();
-imagesArray[3].src = new "alligator.jpg";
-imagesArray[4] = new Image();
-imagesArray[4].src = new "fish.jpg";
-imagesArray[5] = new Image();
-imagesArray[5].src = new "parrot.jpg";
-imagesArray[6] = new Image();
-imagesArray[6].src = new "cat.jpg";
+
+var photoDir = ["../img/BELGIUM.png","../img/BRAZIL.png","../img/FRENCH.png","../img/GERMANY.png","../img/JAPAN.png","../img/USA.png"]
+
+function getPhoto()
+{
+
+}
